d08f104319ee719767ecaa26ccb4a589aa0a6946
leek <1837196750@qq.com>
2021-05-13 10:01:41 +0800

    chore: 添加别名设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import {getAliases} from 'vite-aliases'
+
+const aliases = getAliases({prefix:''})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  resolve:{
+    alias:[...aliases]
+  }
 })
