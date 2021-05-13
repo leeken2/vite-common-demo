fc7c153c452c11673dbc324825bd86a20bcd4236
leek <1837196750@qq.com>
2021-05-13 13:48:01 +0800

    test bad commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!webstorm</p>
+        <p>Hello Vite + React!bad commit</p>
         <p>
           <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
         </p>
